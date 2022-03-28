b131d3f8edd3f18a233fb733f4cc4834256147ae
KarlaG09 <kg02793@srcs.org>
2022-03-28 15:49:27 -0700

    pending

--- Assets/Scenes/Scripts/FollowPlayer.cs
+++ Assets/Scenes/Scripts/FollowPlayer.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     public GameObject player;
+    private Vector3 offset = new Vector3(0, 5, -7);
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +15,11 @@ public class FollowPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = player.transform.position + new Vector3(0, 7, -10);
+        transform.position = player.transform.position + offset;
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
     }
 }
